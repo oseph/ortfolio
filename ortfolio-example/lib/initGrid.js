01f31b5a55b9db7e3badfd0f0709538789ef41ad
Josh Holinaty <holinaty@gmail.com>
2019-08-08 15:21:46 -0400

    fix: thumbnail grid initializes after all images are loaded.

--- ortfolio-example/lib/initGrid.js
+++ ortfolio-example/lib/initGrid.js
@@ -1,12 +1,27 @@
 var grid = document.querySelector('.grid');
 var msnry;
-msnry = new Masonry( grid, {
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-item',
-  gutter:10,
-  percentPosition: true
-});
 
-imagesLoaded( grid ).on( 'progress', function() {
-  msnry.layout();
+// his loads the grid after each thumbnail image is loaded
+// msnry = new Masonry( grid, {
+//   itemSelector: '.grid-item',
+//   columnWidth: '.grid-item',
+//   gutter:10,
+//   percentPosition: true
+// });
+
+// imagesLoaded( grid ).on( 'progress', function() {
+//   // layout Masonry after each image loads
+//   msnry.layout();
+// });
+
+
+// this loads the grid after all thumbnail images are loaded
+imagesLoaded( grid, function() {
+  // init Isotope after all images have loaded
+  msnry = new Masonry( grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-item',
+    gutter:10,
+    percentPosition: true
+  });
 });
